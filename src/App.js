0c74e25f91962df5cb6753de11ede8274073d4e4
Laurie Jefferson <ljdatasci@gmail.com>
2021-01-29 11:09:44 -0500

    removed credentials

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-//import beach1 from "./images/anthony-ingham-beach1-unsplash.jpg";
 import "./App.css";
 const firebaseConfig = {
-  apiKey: "AIzaSyDOMKYSypZPjnslt0tAEs_4kHBe_E4MfgM",
-  authDomain: "image-gallery-93486.firebaseapp.com",
-  projectId: "image-gallery-93486",
-  storageBucket: "image-gallery-93486.appspot.com",
-  messagingSenderId: "201867296521",
-  appId: "1:201867296521:web:78150c53cf14643de99dd8",
-  measurementId: "G-KDXJWNDHDC",
+  //add your credentials here
 };
 
 if (!firebase.apps.length) {
