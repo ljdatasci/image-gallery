c0cffa909f6a5038e5d1ac6702b374ec4316af83
Laurie Jefferson <ljdatasci@gmail.com>
2021-02-08 17:07:48 -0500

    removed firebase initalizeapp config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,7 @@ import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDOMKYSypZPjnslt0tAEs_4kHBe_E4MfgM",
-  authDomain: "image-gallery-93486.firebaseapp.com",
-  projectId: "image-gallery-93486",
-  storageBucket: "image-gallery-93486.appspot.com",
-  messagingSenderId: "201867296521",
-  appId: "1:201867296521:web:78150c53cf14643de99dd8",
-  measurementId: "G-KDXJWNDHDC",
-};
+const firebaseConfig = {};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
